67324ef62894be6504f8dad8163b6009bd4aed65
Valerie Hirsch <9038@htl.rennweg.at>
2025-09-07 12:02:37 +0200

    Aufgabe 2 hardcode not working; Angabe???

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Aufgabe2();
     }
     public static void Aufgabe1 () {
         System.out.println("Hello World!");
     }
 
+    public static void Aufgabe2 () {
+        System.out.println("0123456789012345678901");
 
+        String input ="         __            _(\\    |@@|          (__/\\__ \\--/ __          \\___|----|  |   __        \\ }{ /\\ )_ / _\\ /\\__/\\ \\__O (__         (--/\\--)    \\__/       _)(  )(_              `---''---`        ";
+        String parts[] = new String[input.length()%22];
+
+        for (int i = 0; i < parts.length; i++) {
+            parts[i] = input.substring(22*(i), 22*(i+1));
+        }
+
+        for (int i = 0; i < parts.length; i++) {
+            System.out.println(parts[i]);
+        }
+
+    }
 }
